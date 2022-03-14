c341d79e61d0e4ee277c74f26670a33af64ac6b9
Madyson Bradford <89798314+MadysonBradford@users.noreply.github.com>
2022-03-14 16:22:56 -0500

    start watertable
    placeholder for watertable workflow

---
+++ workflows/watertable.py
@@ -0,0 +1,11 @@
+# calculates water table stats
+
+# characterize inputs into hydrological system
+
+# characterize outputs out-of hydrological system
+
+# calculate water table stats
+
+def watertable():
+    return "nothing yet"
+
